0249925c4edaa47d3fb2d04b5c65ce35f3f31d3c
Jakub Szczepanek <jszczepanek@qualtrics.com>
2022-11-16 22:21:41 +0100

    basic operations

--- cw03/app/src/main/java/com/example/calculator/MainActivity.kt
+++ cw03/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,111 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clearBTN.setOnClickListener {
+            calcSpace.text = ""
+        }
+
+        zeroBTN.setOnClickListener {
+            calcSpace.text = addToInputText("0")
+        }
+
+        oneBTN.setOnClickListener {
+            calcSpace.text = addToInputText("1")
+        }
+
+        twoBTN.setOnClickListener {
+            calcSpace.text = addToInputText("2")
+        }
+
+        threeBTN.setOnClickListener {
+            calcSpace.text = addToInputText("3")
+        }
+
+        fourBTN.setOnClickListener {
+            calcSpace.text = addToInputText("4")
+        }
+
+        fiveBTN.setOnClickListener {
+            calcSpace.text = addToInputText("5")
+        }
+
+        sixBTN.setOnClickListener {
+            calcSpace.text = addToInputText("6")
+        }
+
+        sevenBTN.setOnClickListener {
+            calcSpace.text = addToInputText("7")
+        }
+
+        eightBTN.setOnClickListener {
+            calcSpace.text = addToInputText("8")
+        }
+
+        nineBTN.setOnClickListener {
+            calcSpace.text = addToInputText("9")
+        }
+
+        pointBTN.setOnClickListener {
+            calcSpace.text = addToInputText(".")
+        }
+
+        percentBTN.setOnClickListener {
+            calcSpace.text = addToInputText("%")
+        }
+
+        exponentBTN.setOnClickListener {
+            calcSpace.text = addToInputText("^")
+        }
+
+        divideBTN.setOnClickListener {
+            calcSpace.text = addToInputText("/")
+        }
+
+        multiplyBTN.setOnClickListener {
+            calcSpace.text = addToInputText("*")
+        }
+
+        subtractBTN.setOnClickListener {
+            calcSpace.text = addToInputText("-")
+        }
+
+        addBTN.setOnClickListener {
+            calcSpace.text = addToInputText("+")
+        }
+
+        logBTN.setOnClickListener {
+            calcSpace.text = addToInputText("ln")
+        }
+
+        equalsBTN.setOnClickListener {
+            onEqual()
+        }
     }
+
+    private fun addToInputText(buttonValue: String): String {
+        return "${calcSpace.text}$buttonValue"
+    }
+
+    private fun onEqual() {
+        val expression = ExpressionBuilder(calcSpace.text as String?).build()
+        try {
+            val result = expression.evaluate()
+            println(calcSpace.text as String?)
+            println(expression)
+            println(result)
+            calcSpace.text = result.toString()
+        } catch (ex: ArithmeticException) {
+            calcSpace.text = "Error"
+        }
+    }
+
 }
