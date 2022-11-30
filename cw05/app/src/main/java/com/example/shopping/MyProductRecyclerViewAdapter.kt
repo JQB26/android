1ebb64f09722edfb40534393de0f2e480a50cc39
Jakub Szczepanek <kubaszczepanek@interia.pl>
2022-11-30 13:30:05 +0100

    cart implementation

--- cw05/app/src/main/java/com/example/shopping/MyProductRecyclerViewAdapter.kt
+++ cw05/app/src/main/java/com/example/shopping/MyProductRecyclerViewAdapter.kt
@@ -1,11 +1,12 @@
 package com.example.shopping
 
-import android.content.Context
 import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import com.example.shopping.cart.CartContent
 
 import com.example.shopping.databinding.FragmentProductBinding
 import com.example.shopping.products.ProductsContent
@@ -15,6 +16,8 @@ class MyProductRecyclerViewAdapter(
     private val values: List<ProductsContent.Product>,
     private val clickListener: (String) -> Intent
 ) : RecyclerView.Adapter<MyProductRecyclerViewAdapter.ViewHolder>() {
+
+    private val cart: CartContent = CartContent
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
@@ -36,6 +39,13 @@ class MyProductRecyclerViewAdapter(
         holder.itemView.setOnClickListener {
             clickListener(item.details)
         }
+
+        holder.addToCart.setOnClickListener {
+            cart.addProductToCart(item.id)
+
+            // TODO: remove println when cart activity/view is added
+            println(cart.CART.map{ "${it.key}: ${it.value}" }.joinToString(", "))
+        }
     }
 
     override fun getItemCount(): Int = values.size
@@ -44,6 +54,7 @@ class MyProductRecyclerViewAdapter(
         RecyclerView.ViewHolder(binding.root) {
         val price: TextView = binding.productPrice
         val name: TextView = binding.name
+        val addToCart: Button = binding.addToCart
 
         override fun toString(): String {
             return super.toString() + " '" + name.text + "'"
