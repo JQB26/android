9ee50886983e99dfd0f0a27c85082e3b743c0e7b
Jakub Szczepanek <kubaszczepanek@interia.pl>
2022-12-23 00:06:37 +0100

    code cleanup, models fix

--- shopApp/app/app/src/main/java/com/example/shopping/models/Products.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shopping.models
-
-data class ProductModel(
-    val id: String,
-    val name: String,
-    val price: Float,
-    val category: CategoryModel,
-    val details: String
-)
