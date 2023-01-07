e91cc9797b6b0081bea19a9922e10e0b808d0c7f
Jakub Szczepanek <kubaszczepanek@interia.pl>
2023-01-07 18:55:18 +0100

    error messages displayed test

--- shopApp/app/src/androidTest/java/com/example/shopping/PaymentActivityTest.kt
+++ shopApp/app/src/androidTest/java/com/example/shopping/PaymentActivityTest.kt
@@ -1,5 +1,6 @@
 package com.example.shopping
 
+import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
@@ -8,6 +9,10 @@ import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.material.textfield.TextInputLayout
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -19,7 +24,7 @@ class PaymentActivityTest {
     fun testPaymentActivityGeneral() {
         ActivityScenario.launch(PaymentActivity::class.java)
 
-        checkAllTheFields()
+        checkAllTheFieldsDisplayed()
     }
 
     @Test
@@ -32,11 +37,23 @@ class PaymentActivityTest {
         acceptPaymentButton.perform(ViewActions.click())
 
         Intents.intended(IntentMatchers.hasComponent(PaymentActivity::class.java.name))
+        checkAllTheFieldsDisplayed()
 
-        checkAllTheFields()
+        onView(withId(R.id.textInputSurname)).check(
+            matches(hasTextInputLayoutHintText("Field is required"))
+        )
+        onView(withId(R.id.textInputName)).check(
+            matches(hasTextInputLayoutHintText("Field is required"))
+        )
+        onView(withId(R.id.textInputCardNumber)).check(
+            matches(hasTextInputLayoutHintText("Field is required"))
+        )
+        onView(withId(R.id.textInputCVV)).check(
+            matches(hasTextInputLayoutHintText("Field is required"))
+        )
     }
 
-    private fun checkAllTheFields() {
+    private fun checkAllTheFieldsDisplayed() {
         onView(withId(R.id.textInputSurname)).check(matches(isDisplayed()))
         onView(withId(R.id.textInputSurname))
             .check(matches(hasDescendant(withHint(R.string.surname))))
@@ -64,3 +81,16 @@ class PaymentActivityTest {
         onView(withId(R.id.total_price_text)).check(matches(withText(R.string.total_price_is)))
     }
 }
+
+fun hasTextInputLayoutHintText(expectedErrorText: String): Matcher<View> = object : TypeSafeMatcher<View>() {
+
+    override fun describeTo(description: Description?) { }
+
+    override fun matchesSafely(item: View?): Boolean {
+        if (item !is TextInputLayout) return false
+        val error = item.error ?: return false
+        val hint = error.toString()
+        return expectedErrorText == hint
+    }
+}
+
